1cd218c5ea07682edaba2337f5b0a9c05ee85a7c
TalentDevking <talentdev311@outlook.com>
2024-06-09 19:44:24 +0400

    fea: lonely integer

---
+++ Interview Preparation Kits/1 Week Preparation Kit/Day 1/Lonely Integer.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    d = {}
+    for value in a:
+        if value not in d:
+            d[value] = 1
+        else:
+            d[value] = d[value] + 1
+    for key in d:
+        if d[key] == 1:
+            return key
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
